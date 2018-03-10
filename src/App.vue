43f70570892d4ddeceebf2c5c3a2f4b495a09c75
Juha Katila <juha.katila@ambientia.fi>
2018-03-10 13:47:19 +0200

    scss divided to own files + logo now in header

--- src/App.vue
+++ src/App.vue
@@ -20,24 +20,8 @@
 
 <style lang="scss">
 
-$amb-color-red: rgb(255, 36, 87);
-$amb-color-orange: rgb(255, 110, 0);
-$amb-color-green: rgb(80, 255, 126);
-$amb-color-blue: rgb(5, 5, 255);
-$amb-color-darkblue: rgb(0, 0, 102);
-$amb-color-magenta: rgb(212, 0, 172);
-$amb-color-black: rgb(0, 0, 0);
+@import "/assets/styles/_global.scss";
 
-html, body {
-  margin: 0;
-  padding: 0;
-}
-
-#app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-  }
+@import "/assets/styles/_base.scss";
 
 </style>
--- src/views/input/ExerciseForm.vue
+++ src/views/input/ExerciseForm.vue
@@ -1,7 +1,11 @@
 <template>
 
   <div class="componentContainer">
-    <h1>Holaxxxx!</h1>
+    <header>
+      <img src="../../assets/ambientia-logo.svg">
+      <h1>UFTC XIII</h1>
+    </header>
+
     <exercise-selector :exercise="exercise"></exercise-selector>
   </div>
 </template>
@@ -28,5 +32,9 @@
 </script>
 
 <style lang="scss" scoped>
+
+@import "../../assets/styles/_global.scss";
+
+@import "../../assets/styles/_header.scss";
 
 </style>
