edd82815f7f97815a3a35870400c8377948be1e2
Juha Katila <juha.katila@ambientia.fi>
2018-03-10 18:19:09 +0200

    daily score cleaned + summary modifications

--- src/components/ExerciseInput.vue
+++ src/components/ExerciseInput.vue
@@ -4,7 +4,7 @@
     <h2>Lisää valitun suorituksen tiedot</h2>
     <div class="input-container">
       <div class="input-selected-name">{{ selectedExercise.name }}</div>
-      <input class="input-number" @keyup.enter="addExercise" type="text" placeholder="0" v-model="units"/>
+      <input class="input-number" @keyup.enter="addExercise" type="number" placeholder="0" v-model="units"/>
       <div class="input-unit">{{ selectedExercise.unit }}</div>
       <div class="input-details">Pisteitä {{ selectedExercise.pointsPerUnit}} per {{ selectedExercise.unit }}</div>
       <button @click="addExercise">Lisää päivän suorituksiin<span class="forward"></span></button>
