0e24be64ff9bd6427e59bc0acb60ea907bd2c3c4
Lauri Hiltunen <hiltusenlauri@gmail.com>
2018-04-02 21:17:21 +0300

    Exerciseinput localization

--- src/components/ExerciseInput.vue
+++ src/components/ExerciseInput.vue
@@ -1,12 +1,12 @@
 <template>
 
   <div v-if="selectedExercise">
-    <h2>Lisää valitun suorituksen tiedot</h2>
+    <h2>{{ "scorecard-add-data" | translate }}</h2>
     <div class="input-container">
-      <div class="input-selected-name">{{ selectedExercise.name }}</div>
+      <div class="input-selected-name">{{ 'exercise-'+selectedExercise.name | translate }}</div>
       <input class="input-number" @keyup.enter="addExercise" type="number" placeholder="0" v-model="units"/>
       <div class="input-unit">{{ selectedExercise.unit }}</div>
-      <div class="input-details">Pisteitä {{ selectedExercise.pointsPerUnit}} per {{ selectedExercise.unit }}</div>
+      <div class="input-details">{{ $t('exercise-gives-amount-points-for-every-unit', { amount: selectedExercise.pointsPerUnit, unit: selectedExercise.unit }) }}</div>
       <button @click="addExercise">Lisää päivän suorituksiin<span class="forward"></span></button>
     </div>
   </div>
