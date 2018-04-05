eb133b0ff0748440af668c43198d95ab5115dc83
Lauri Hiltunen <hiltusenlauri@gmail.com>
2018-04-05 19:12:36 +0300

    Fetching challenges and listing them

--- src/services/challenge/ChallengeService.js
+++ src/services/challenge/ChallengeService.js
@@ -1,10 +1,26 @@
-import ExerciseService from '../exercises/ExerciseService'
+const axios = require('axios');
 
 class ChallengeService {
 
   constructor() {
 
   }
+
+  getChallenges(){
+
+    return new Promise(function(resolve, reject) {
+
+      axios.get('http://localhost:3000/challenges')
+        .then(response => {
+          resolve(response.data)
+        })
+        .catch(error => {
+          reject(error)
+        });
+
+    })
+  }
+
 
 }
 
--- src/views/admin/ChallengeAdmin.vue
+++ src/views/admin/ChallengeAdmin.vue
@@ -2,18 +2,45 @@
 
   <div class="componentContainer">
     <p>Challenge admin</p>
+    <ul>
+      <li v-for="challenge in challenges">
+        {{ challenge.name }}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 
+  import ChallengeService from 'services/challenge/ChallengeService'
+  
   export default {
       name: "challenge-admin",
       data() {
         return {
-
+          challenges: [],
+          error: false,
+          errorMessage: ''
         }
+      },
+    methods: {
+      getChallenges() {
+        ChallengeService.getChallenges().then(
+          function(data) {
+            data.forEach(challenge => {
+              this.challenges.push(challenge)
+            })
+          }.bind(this),
+          function(){
+            this.error = true
+            this.errorMessage = 'Unable to get challenges'
+          }.bind(this)
+        )
       }
+    },
+    mounted() {
+        this.getChallenges()
+    }
     }
 </script>
 
