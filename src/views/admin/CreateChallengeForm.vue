92a76b0ac66beffcb1c761f970f15030506d4d47
Lauri Hiltunen <hiltusenlauri@gmail.com>
2018-04-07 19:11:23 +0300

    Exercises listed for new exercise form

--- src/views/admin/CreateChallengeForm.vue
+++ src/views/admin/CreateChallengeForm.vue
@@ -4,15 +4,37 @@
     <main>
       <div class="form-challenge">
         <input placeholder="">
+        <h3>Exercises to include:</h3>
+        <ul>
+          <li v-for="exercise in exercises">{{ exercise.name }}</li>
+        </ul>
       </div>
     </main>
   </div>
 </template>
 
 <script>
-    export default {
-        name: "create-challenge-form"
+  import ExerciseService from 'services/exercises/ExerciseService'
+  
+  export default {
+      name: "create-challenge-form",
+      data: function() {
+        return {
+          exercises: []
+        }
+      },
+      mounted(){
+        
+        ExerciseService.getExercises().then(
+          function(data) {
+            data.forEach(exercise => {
+              this.exercises.push(exercise)
+            })
+          }.bind(this)
+        )
+      }
     }
+
 </script>
 
 <style scoped>
