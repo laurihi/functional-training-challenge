853654e0bbf00bbd779ae4811c0dac4a46374481
Lauri Hiltunen <hiltusenlauri@gmail.com>
2018-04-08 15:00:10 +0300

    Add includible-exercise component for creating new challenge

---
+++ src/components/admin/IncludibleExercise.vue
@@ -0,0 +1,51 @@
+<template>
+  
+  <div class="exercise-row">
+    <span>
+      <input type="checkbox"/>
+    </span>
+    <span>
+      {{ 'exercise-'+exercise.name | translate }}
+    </span>
+    <span>
+      <input type="number">
+    </span>
+   </div>
+
+</template>
+
+<script>
+  import { mapActions, mapGetters } from 'Vuex'
+  
+  export default {
+    
+    name: "exercise-selector",
+    props: {
+      exercise: {
+        type: Object
+      }
+    },
+    methods: {
+      selectExercise() {
+        console.log('Selecting exercise ' + this.exercise.name)
+        this.selectActiveExercise(this.exercise)
+      },
+      ...mapActions( {
+        selectActiveExercise: 'selectActiveExercise'
+      } )
+    },
+    computed: {
+      isCurrentlyActive() {
+        return this.selectedExercise != undefined && this.selectedExercise === this.exercise;
+      },
+      ...mapGetters({
+        selectedExercise: 'selectedExercise'
+      })
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+
+</style>
--- src/views/admin/CreateChallengeForm.vue
+++ src/views/admin/CreateChallengeForm.vue
@@ -1,42 +1,42 @@
 <template>
   <div class="componentContainer">
-    <h3>Luo uusi haaste</h3>
-    <main>
-      <div class="form-challenge">
-        <input placeholder="">
-        <h3>Exercises to include:</h3>
-        <ul>
-          <li v-for="exercise in exercises">{{ exercise.name }}</li>
-        </ul>
-      </div>
-    </main>
+    <h3>{{ 'header-create-new-challenge' | translate }}</h3>
+    <div class="form-challenge container">
+      <input placeholder="">
+      <h3>Exercises to include:</h3>
+      <includible-exercise :exercise="exercise" v-for="exercise in exercises"></includible-exercise>
+    </div>
   </div>
 </template>
 
 <script>
   import ExerciseService from 'services/exercises/ExerciseService'
+  import IncludibleExercise from 'components/admin/IncludibleExercise'
   
   export default {
-      name: "create-challenge-form",
-      data: function() {
-        return {
-          exercises: []
-        }
-      },
-      mounted(){
-        
-        ExerciseService.getExercises().then(
-          function(data) {
-            data.forEach(exercise => {
-              this.exercises.push(exercise)
-            })
-          }.bind(this)
-        )
+    name: "create-challenge-form",
+    data: function() {
+      return {
+        exercises: []
       }
+    },
+    components: {
+      'includible-exercise': IncludibleExercise
+    },
+    mounted() {
+      ExerciseService.getExercises().then(
+        function(data) {
+          data.forEach(exercise => {
+            this.exercises.push(exercise)
+          })
+        }.bind(this)
+      )
     }
+  }
 
 </script>
 
-<style scoped>
+<style>
+
 
 </style>
