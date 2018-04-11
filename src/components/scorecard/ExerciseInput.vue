216322a0f7eaf1d5be772790858e265bbd06e3db
Lauri Hiltunen <hiltusenlauri@gmail.com>
2018-04-11 21:43:23 +0300

    PoC for selecting date as a part of exercise input.

--- src/components/scorecard/ExerciseInput.vue
+++ src/components/scorecard/ExerciseInput.vue
@@ -7,19 +7,25 @@
       <input class="input-number" @keyup.enter="addExercise" type="number" placeholder="0" v-model="units"/>
       <div class="input-unit">{{ selectedExercise.unit }}</div>
       <div class="input-details">{{ $t('exercise-gives-amount-points-for-every-unit', { amount: selectedExercise.pointsPerUnit, unit: selectedExercise.unit }) }}</div>
+      <div class="input-details">{{ 'Exercises done on' }}</div>
+      <datepicker wrapper-class="date-selector-wrapper" input-class="date-selector" v-model="date" name="date" placeholder="" ></datepicker>
+  
       <button @click="addExercise">{{ 'actions-exercise-add-to-daily-score' | translate }}<span class="forward"></span></button>
     </div>
   </div>
 </template>
 
 <script>
-  import { mapActions, mapGetters } from 'Vuex'
+
+  import { mapActions, mapGetters } from 'vuex'
+  import Datepicker from 'vuejs-datepicker';
 
   export default {
         name: "exercise-input",
         data: () => {
           return {
-            units: 0
+            units: 0,
+            date: '11.04.'
           }
         },
         methods: {
@@ -39,6 +45,9 @@
           ...mapGetters([
             'selectedExercise'
           ])
+        },
+        components: {
+          'datepicker': Datepicker
         }
     }
 </script>
