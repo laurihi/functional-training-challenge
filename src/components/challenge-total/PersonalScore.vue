cbd024c04544f5c30507c3699bd52a19c7be419a
Lauri Hiltunen <hiltusenlauri@gmail.com>
2018-04-16 21:53:07 +0300

    PersonalScore component added with some mock data

---
+++ src/components/challenge-total/PersonalScore.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="component-container">
+      <section class="score-total-wrapper">
+        <p>You've got...</p>
+        <h3>3500 points!</h3>
+        <p>Currently you're on Xth place</p>
+        <hr/>
+        <p>X points to move up a position</p>
+        <hr/>
+        <p>Y points to the top</p>
+        <hr/>
+        <p>Matt is trying to catch up with you, he's only Z points behind</p>
+        <hr/>
+        <a>Add new exercises</a>
+      </section>
+
+    </div>
+</template>
+
+<script>
+  export default {
+    name: "personal-score"
+  }
+</script>
+
+<style scoped lang="scss">
+  .score-total-wrapper {
+    width: 30%;
+    text-align: center;
+    border: crimson dashed thin;
+    
+    p {
+      margin: 0px 0px;
+    }
+    
+    hr {
+        border: 0;
+        height: 1px;
+        background-image: linear-gradient(to right, rgba(248, 12, 58, 0), rgba(248, 12, 58, 1), rgba(248, 12, 58, 0));
+    }
+  }
+</style>
