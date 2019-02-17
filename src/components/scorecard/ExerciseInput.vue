461deb0db35b9927b7d2c72ae2fb938c9bdfc772
Lauri Hiltunen <hiltusenlauri@gmail.com>
2019-02-17 16:48:32 +0200

    Remove redundant local method on date select

--- src/components/scorecard/ExerciseInput.vue
+++ src/components/scorecard/ExerciseInput.vue
@@ -5,9 +5,10 @@
     <div class="input-container">
       <div class="input-selected-name">{{ 'exercise-'+selectedExercise.exerciseKey | translate }}</div>
 
-      <datepicker wrapper-class="date-selector-wrapper" input-class="date-selector" :value="selectedDate" @selected="setDate" :format="'date-format' | translate" :language="$i18n.locale()"></datepicker>
+      <datepicker wrapper-class="date-selector-wrapper" input-class="date-selector" :value="selectedDate" @selected="selectDate" :format="'date-format' | translate" :language="$i18n.locale()"></datepicker>
+      
       <input class="input-number" @keyup.enter="addExercise" type="number" placeholder="0" v-model="units"/>
-
+      
       <div class="input-unit">{{ selectedExercise.unit }}</div>
       <div class="input-details">{{ $t('exercise-gives-amount-points-for-every-unit', { amount: selectedExercise.pointsPerUnit, unit: selectedExercise.unit }) }}</div>
       <button @click="addExercise">{{ 'actions-exercise-add-to-daily-score' | translate }}<span class="forward"></span></button>
@@ -29,16 +30,13 @@
         },
         methods: {
           addExercise() {
-            
+
             const payload = {
               units: this.units,
               exercise: this.selectedExercise,
               date: this.selectedDate
             }
             this.addDailyExercises(payload)
-          },
-          setDate(date){
-            this.selectDate(date)
           },
           ...mapActions( {
             addDailyExercises: 'addDailyExercises',
